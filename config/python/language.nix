b1a145faaad54dd6564365cef803fc36e79eea9f
jerrydzhang <jerry.zhang.d@gmail.com>
2025-02-26 20:28:55 -0500

    python formatting

--- config/python/language.nix
+++ config/python/language.nix
@@ -1,8 +1,22 @@
 {
-plugins = {
+  plugins = {
+    conform-nvim = {
+      enable = true;
+      settings = {
+        format_on_save = {
+          lsp_fallback = "fallback";
+          timeout_ms = 500;
+        };
+        notify_on_error = true;
+
+        formatters_by_ft = {
+          nix = ["alejandra"];
+        };
+      };
+    };
     lsp.servers.pyright = {
       enable = true;
     };
     none-ls.sources.formatting.black.enable = true;
-	};
+  };
 }
